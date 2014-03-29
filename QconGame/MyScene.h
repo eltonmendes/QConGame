48c2c27fb9625771704f1e26d8a54172b0140abf
Elton Mendes Vieira Junior <eltonj@maccit007474.local>
2014-03-29 11:32:00 -0300

    Initial Commit

---
+++ QconGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  QconGame
+//
+
+//  Copyright (c) 2014 CI&T. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ QconGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QconGame
+//
+//  Created by Elton Mendes Vieira Junior on 29/03/14.
+//  Copyright (c) 2014 CI&T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
